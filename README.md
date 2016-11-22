07a19a7ec67305383dbe6b51d25735594d0fbebe
NoahTK7 <bontkurrack@gmail.com>
2016-11-22 17:39:06 -0500

    update font awesome, include libs through inject.js first icons, outline of features in readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # GradeEditor
 A web browser extension for termporarily editing grades on the Pinnacle website.
+
+### outline of planned features
+- edit current assignments
+- delete current assignments
+- add new assignments
+- reset button (page,existing assignments)
+
+- lock current state (local storage) - "parent mode"
+
+- edit grade summary page
+
+- attendance?
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,12 +8,46 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("[GradeEditor] Script successfully injected.");
 		// ----------------------------------------------------------
 
+    // dependencies
+    var head = document.getElementsByTagName('head')[0];
+
+    if (head) {
+      var fontawesome = document.createElement('link');
+      fontawesome.rel = 'stylesheet';
+      fontawesome.href = chrome.extension.getURL('assets/libs/font-awesome-4.7.0/css/font-awesome.min.css');
+      
+      var jquery = document.createElement('script');
+      jquery.type = 'text/javascript';
+      jquery.src = chrome.extension.getURL('assets/libs/jquery-3.1.1.min.js');
+      
+      head.appendChild(fontawesome);
+      head.appendChild(jquery);
+    }
+    // document.write("<script src='https://use.fontawesome.com/c01af75451.js'></script><script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>");
+    
 		//do stuff?
+    addButtons();
 
 	}
 	}, 10);
 });
 
+function addButtons(){
+  
+  var assignments = document.getElementsByClassName("assignment");
+  
+  for (var i = 0; i < assignments.length; i++) {
+    var editElement = document.createElement("td");
+    editElement.setAttribute("class", "editIconsContainer");
+  
+    var editIconElement = document.createElement("span");
+    editIconElement.setAttribute("class", "fa fa-pencil-square-o");
+    editElement.appendChild(editIconElement);
+    
+    assignments[i].appendChild(editElement);
+  }
+  
+}
 
 /* get message from page action (from SCStreamModifier)
 chrome.runtime.onMessage.addListener(
